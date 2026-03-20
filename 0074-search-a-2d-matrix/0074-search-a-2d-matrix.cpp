5fde74d8319bd4df5f51efe9bc184c5a6b479334
PRADEEP RAJ <139350539+bpradeepraj2005@users.noreply.github.com>
2026-03-20 20:51:10 +0530

    Time: 1 ms (2.43%), Space: 13.3 MB (78.2%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,13 +1,45 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int row=0; row<matrix.size(); row++){
-            for (int col=0; col<matrix[row].size(); col++){
-                if(target == matrix[row][col]){
-                    return true;
-                }
+    bool bs(vector<vector<int>>&matrix, int low, int st, int ed, int target){
+        
+        if(matrix[low][st]<=target && target<=matrix[low][ed]){
+            int mid=st+(ed-st)/2;
+
+            if(matrix[low][mid]== target) return true;
+
+            else if(matrix[low][mid]>target){
+                return bs(matrix, low, st, mid-1, target);
+            }
+            else{
+               return bs(matrix, low, mid+1, ed, target);
             }
         }
         return false;
+
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // for(int row=0; row<matrix.size(); row++){
+        //     for (int col=0; col<matrix[row].size(); col++){
+        //         if(target == matrix[row][col]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        
+
+        for(int i=0; i<matrix.size(); i++){
+            int low=i, st, ed;
+            // int high=matrix.size()-1;
+            
+            if(true == bs(matrix, low, st=0, ed=matrix[i].size()-1, target))
+                return true;
+        }
+
+        return false;
+
+
     }
 };
