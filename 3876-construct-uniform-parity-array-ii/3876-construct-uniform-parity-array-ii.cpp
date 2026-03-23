7da3039ffc3fa5811526f2e5c6d48fa7f9d4bacb
PRADEEP RAJ <139350539+bpradeepraj2005@users.noreply.github.com>
2026-03-24 00:22:57 +0530

    Time: 71 ms (7.14%), Space: 165.9 MB (7.14%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int ev=0, odd=0, mini=1e8;
+
+        for(int i=0; i<nums1.size(); i++){
+            if(nums1[i]%2==0) ev++;
+            else {
+                odd++;
+                mini=min(mini, nums1[i]);
+            }
+        }
+
+        sort(nums1.begin(), nums1.end());    
+
+
+        if(odd == nums1.size() || ev == nums1.size()) return true;    
+        
+   
+        for(int i=0; i<nums1.size(); i++){
+            if(nums1[i]%2==0 && (nums1[i]-mini <=0) ){
+                return false;
+            }
+        }
+
+        return true;
+        
+        
+    }
+};
